392f84131d50206dfb4f5703cf2fa35c2ef4f8c4
Shirin Etezazian <64745415+setezazian@users.noreply.github.com>
2021-12-12 22:46:39 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+  },
+  plugins: [
+  ],
+  rules: {
+  },
+};
---
+++ index.js
@@ -0,0 +1,4 @@
+const someFunctionName = () => {
+};
+
+module.exports = someFunctionName;
